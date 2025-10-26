c45c0214132161f5de32da106bc3d7f6a4e14c79
Shinobu Oshino :3 <43119329+Shinobuos@users.noreply.github.com>
2025-10-25 19:33:19 -0500

    [encode] Gahhh

--- 03.py
+++ 03.py
@@ -19,7 +19,7 @@ else:
     assert "SOURCE_DIR" in os.environ, "You need to either pass the directory containing the sources via commandline parameters `--source-dir`, or via environmental variable \"SOURCE_DIR\""
     source_dir = SPath(os.environ["SOURCE_DIR"])
 
-episode = "04"
+episode = "03"
 source = source_dir / f"[VARYG] {episode}.mkv"
 trim = (120, None)
 
@@ -30,4 +30,5 @@ if not is_preview():
         mux(episode, filterchain_results)
     else:
         filterchain_results.final.set_output()
+
 
