99f69365e5ccbcda3ff017f52976b16c4415ca74
Shinobu Oshino :3 <43119329+Shinobuos@users.noreply.github.com>
2025-10-25 19:28:33 -0500

    Update episode number to 04 in 04.py

--- 04.py
+++ 04.py
@@ -19,7 +19,7 @@ else:
     assert "SOURCE_DIR" in os.environ, "You need to either pass the directory containing the sources via commandline parameters `--source-dir`, or via environmental variable \"SOURCE_DIR\""
     source_dir = SPath(os.environ["SOURCE_DIR"])
 
-episode = "03"
+episode = "04"
 source = source_dir / f"[VARYG] {episode}.mkv"
 trim = (120, None)
 
@@ -30,3 +30,4 @@ if not is_preview():
         mux(episode, filterchain_results)
     else:
         filterchain_results.final.set_output()
+
