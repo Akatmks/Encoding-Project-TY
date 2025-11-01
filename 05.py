570cab865759ea3742738c502698b4b53107a800
Akatsumekusa <Akatsumekusa@protonmail.com>
2025-11-01 18:21:55 +0000

    [encode] Fix 05

--- 05.py
+++ 05.py
@@ -19,7 +19,7 @@ else:
     assert "SOURCE_DIR" in os.environ, "You need to either pass the directory containing the sources via commandline parameters `--source-dir`, or via environmental variable \"SOURCE_DIR\""
     source_dir = SPath(os.environ["SOURCE_DIR"])
 
-episode = "04"
+episode = "05"
 source = source_dir / f"[VARYG] {episode}.mkv"
 trim = (120, None)
 
